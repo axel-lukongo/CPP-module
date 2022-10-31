92931b67af371f059df233d74c3a69e487a63da5
axel lukongo <axellukongo@MacBook-Air-de-axel.local>
2022-10-31 18:34:57 +0100

    cpp

--- day01/ex04/main.cpp
+++ day01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alukongo <alukongo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 19:35:23 by alukongo          #+#    #+#             */
-/*   Updated: 2022/10/31 16:29:24 by alukongo         ###   ########.fr       */
+/*   Updated: 2022/10/31 18:33:34 by alukongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,27 +27,29 @@ int main(int ac, char **av){
 		name = av[1];
 		std::string str1 = av[2];
 		std::string str2 = av[3];
-		std::ofstream file2 (name.substr(0, name.size()).append(".replace"));
-
-		while (getline(file1, buf))
-			tmp += buf + '\n';
-
-		if(str1 != str2)
+		if (file1.is_open())
 		{
-			while(1)
+			std::ofstream file2 (name.substr(0, name.size()).append(".replace"));
+			while (getline(file1, buf))
+				tmp += buf + '\n';
+
+			if(str1 != str2)
 			{
-				int i = tmp.find(str1);
-				if (i >= std::string::npos)
-					break;
-				tmp.erase(i, str1.size());
-				buf = tmp.insert(i, str2);
+				while(1)
+				{
+					int i = tmp.find(str1);
+					if (i >= std::string::npos)
+						break;
+					tmp.erase(i, str1.size());
+					buf = tmp.insert(i, str2);
+				}
 			}
+			else
+				buf = tmp;
+			file2 << buf;
+			file1.close();
+			file2.close();
 		}
-		else
-			buf = tmp;
-		file2 << buf;
-		file1.close();
-		file2.close();
 	}
 	else
 		std::cout << "wrong argument" << std::endl;
